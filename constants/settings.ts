8e6413a7cef41a9bac724198d3aee21b0f2a4efc
Vinicius <vini2143vinicius@gmail.com>
2025-11-17 08:01:33 -0300

    chore: add build folders in gititnore

--- constants/settings.ts
+++ constants/settings.ts
@@ -1,2 +1,2 @@
-export const HTTP_API_URL = "http://10.10.150.59:8000/api/v1"
-export const WS_API_URL = "ws://10.10.150.59:8000/api/v1"
+export const HTTP_API_URL = "http://localhost:8000/api/v1"
+export const WS_API_URL = "http://localhost:8000"
