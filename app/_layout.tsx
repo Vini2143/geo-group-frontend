21a831f59e658c4482b70f7549ed212bd3131180
Vinicius <vini2143vinicius@gmail.com>
2025-11-17 08:02:45 -0300

    feat: add titles in routes home and map

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="home"
+        options={{ title: "Meus Grupos" }}
+      />
+      <Stack.Screen
+        name="map"
+        options={{ title: "Mapa" }}
+      />
+    </Stack>
+  )
 }
